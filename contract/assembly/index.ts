db45bb49e59a4f096d16cc8b1412424dcf3477f0
Karen Bie <karen_bie@hotmail.com>
2021-07-17 19:40:13 -0400

    Refactor contract to follow singleton method

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -16,25 +16,58 @@ import { Context, logging, storage } from 'near-sdk-as'
 
 const DEFAULT_MESSAGE = 'Hello'
 
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
-export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
+@nearBindgen
+export class Contract {
+
+  /* TODO: REQUIRES IMPLEMENTATION FOR "what's up?"
+   *
+   * 1. Get top news items (i.e. news items ordered by votes)
+   * 2. Get all news items ⭐ (maybe goes with the first one?)
+   * 3. Post news item (params e.g. summary string, link, account id)
+   *    defaults to be set: id=genereated value, votes=0 ⭐
+   * 4. Get news items by account id
+   * 5. Vote for a news item (params e.g. news item id, vouch amount)
+   *    defaults to be set: 0.22
+   * 6. Get top news by date (i.e. news items ordered by votes)
+   * 7. todo: all the logic with distributing rewards
+   */
+
+  // "Hello World" with singleton method
+
+  getGreeting(accountId: string): string | null {
+    return storage.get<string>(accountId, DEFAULT_MESSAGE);
+  }
+
+  setGreeting(message: string): void {
+    const account_id = Context.sender
+
+    logging.log('Saving greeting "' + message + '" for account "' + account_id + '"')
+
+    storage.set(account_id, message)
+  }
 }
 
-export function setGreeting(message: string): void {
-  const account_id = Context.sender
+// "Hello World" example with simple export method
 
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(
-    // String interpolation (`like ${this}`) is a work in progress:
-    // https://github.com/AssemblyScript/assemblyscript/pull/1115
-    'Saving greeting "' + message + '" for account "' + account_id + '"'
-  )
+// // Exported functions will be part of the public interface for your smart contract.
+// // Feel free to extract behavior to non-exported functions!
+// export function getGreeting(accountId: string): string | null {
+//   // This uses raw `storage.get`, a low-level way to interact with on-chain
+//   // storage for simple contracts.
+//   // If you have something more complex, check out persistent collections:
+//   // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
+//   return storage.get<string>(accountId, DEFAULT_MESSAGE)
+// }
 
-  storage.set(account_id, message)
-}
+// export function setGreeting(message: string): void {
+//   const account_id = Context.sender
+
+//   // Use logging.log to record logs permanently to the blockchain!
+//   logging.log(
+//     // String interpolation (`like ${this}`) is a work in progress:
+//     // https://github.com/AssemblyScript/assemblyscript/pull/1115
+//     'Saving greeting "' + message + '" for account "' + account_id + '"'
+//   )
+
+//   storage.set(account_id, message)
+// }

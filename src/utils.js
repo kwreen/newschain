7bf6fc6cbcda361e33a255df9110b8c4bca4c3c9
Karen Bie <karen.bie@shopify.com>
2021-07-24 18:04:43 -0400

    Implement work to getNewsItems and createNewsItem

--- src/utils.js
+++ src/utils.js
@@ -17,10 +17,12 @@ export async function initContract() {
 
   // Initializing our contract APIs by contract name and configuration
   window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
+    // todo: tmp code
+
     // View methods are read only. They don't modify the state, but usually return some value.
-    viewMethods: ['getGreeting'],
+    viewMethods: ['getGreeting', "getNewsItems"],
     // Change methods can modify the state. But you don't receive the returned value when called.
-    changeMethods: ['setGreeting'],
+    changeMethods: ["createNewsItem"],
   })
 }
 

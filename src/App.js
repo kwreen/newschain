d1cc49a3e6ce98f2223c25a31a5574f40423823e
Karen Bie <karen_bie@hotmail.com>
2021-07-30 01:18:23 -0400

    Refactor App.js to use React Router and Bootstrap
    + Create empty components for CreateNewsItem and About pages
    + Set up basic navbar
    + Bring back setGreeting change method for testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "regenerator-runtime/runtime";
-import React from "react";
+import React, { useRef } from "react";
 import { login, logout } from "./utils";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./global.css";
 
-import ExampleHome from "./Components/ExampleHome";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+  Container,
+  Navbar,
+  Nav,
+  Row,
+  Card,
+  Form,
+  Button,
+} from "react-bootstrap";
+
 import ExampleIntro from "./Components/ExampleIntro";
 import ExampleNotification from "./Components/ExampleNotification";
+
+import CreateNewsItem from "./Components/CreateNewsItem";
+import About from "./Components/About";
 
 export default function App() {
   // use React Hooks to store greeting in component state
@@ -19,29 +34,22 @@ export default function App() {
 
   // The useEffect hook can be used to fire side-effects during render
   // Learn more: https://reactjs.org/docs/hooks-intro.html
-  React.useEffect(
-    () => {
-      // in this case, we only care to query the contract when signed in
-      if (window.walletConnection.isSignedIn()) {
-        // window.contract is set by initContract in index.js
-        window.contract
-          .getGreeting({ accountId: window.accountId })
-          .then((greetingFromContract) => {
-            setGreeting(greetingFromContract);
-          });
-
-        // todo: tmp code
-        window.contract.getNewsItems().then((newsItems) => {
-          console.log(newsItems);
+  React.useEffect(() => {
+    // in this case, we only care to query the contract when signed in
+    if (window.walletConnection.isSignedIn()) {
+      // window.contract is set by initContract in index.js
+      window.contract
+        .getGreeting({ accountId: window.accountId })
+        .then((greetingFromContract) => {
+          setGreeting(greetingFromContract);
         });
-      }
-    },
 
-    // The second argument to useEffect tells React when to re-run the effect
-    // Use an empty array to specify "only run on first render"
-    // This works because signing into NEAR Wallet reloads the page
-    []
-  );
+      // todo: tmp code
+      window.contract.getNewsItems().then((newsItems) => {
+        console.log(newsItems);
+      });
+    }
+  }, []);
 
   // if not signed in, return early with sign-in prompt
   if (!window.walletConnection.isSignedIn()) {
@@ -55,108 +63,136 @@ export default function App() {
     );
   }
 
+  const greetingField = useRef("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
+    const newGreeting = greetingField.current.value;
+
+    // disable the form while the value gets updated on-chain
+    setButtonDisabled(true);
+
+    try {
+      // make an update call to the smart contract
+      await window.contract.setGreeting({
+        // pass the value that the user entered in the greeting field
+        message: newGreeting,
+      });
+
+      // todo: tmp code
+      // await window.contract.createNewsItem({
+      //   title: newGreeting,
+      //   link: "google.com/xd",
+      // });
+    } catch (e) {
+      alert(
+        "Something went wrong! " +
+          "Maybe you need to sign out and back in? " +
+          "Check your browser console for more info."
+      );
+      throw e;
+    } finally {
+      // re-enable the form, whether the call succeeded or failed
+      setButtonDisabled(false);
+    }
+
+    // update local `greeting` variable to match persisted value
+    setGreeting(newGreeting);
+
+    setShowNotification(true);
+
+    // remove Notification again after css animation completes
+    // this allows it to be shown again next time the form is submitted
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 11000);
+  };
+
   return (
-    <>
+    <BrowserRouter>
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Newschain</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav.Link href="/create">Create</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link
+              onClick={window.walletConnection.isSignedIn() ? logout : login}
+            >
+              {/* add code here for login */}
+              {window.walletConnection.isSignedIn()
+                ? window.accountId
+                : "Login"}
+            </Nav.Link>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        {window.walletConnection.isSignedIn() ? (
+          <Row
+            className="d-flex justify-content-center"
+            style={{ marginTop: "10px" }}
+          >
+            <Switch>
+              <Route exact path="/create">
+                <CreateNewsItem />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </Row>
+        ) : (
+          <Row className="d-flex justify-content-center">
+            <Card>
+              <Card.Body>
+                {" "}
+                <Card.Title>Please Sign In</Card.Title>
+              </Card.Body>
+            </Card>
+          </Row>
+        )}
+      </Container>
+
       <button className="link" style={{ float: "right" }} onClick={logout}>
         Sign out
       </button>
+
       <main>
-        <h1>
-          <label
-            htmlFor="greeting"
-            style={{
-              color: "var(--secondary)",
-              borderBottom: "2px solid var(--secondary)",
-            }}
-          >
-            {greeting}
-          </label>
-          {
-            " " /* React trims whitespace around tags; insert literal space character when needed */
-          }
-          {window.accountId}!
-        </h1>
-        <form
-          onSubmit={async (event) => {
-            event.preventDefault();
-
-            // get elements from the form using their id attribute
-            const { fieldset, greeting } = event.target.elements;
-
-            // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
-            const newGreeting = greeting.value;
-
-            // disable the form while the value gets updated on-chain
-            fieldset.disabled = true;
-
-            try {
-              // make an update call to the smart contract
-              await window.contract.setGreeting({
-                // pass the value that the user entered in the greeting field
-                message: newGreeting,
-              });
-
-              // todo: tmp code
-              await window.contract.createNewsItem({
-                title: newGreeting,
-                link: "google.com/xd",
-              });
-            } catch (e) {
-              alert(
-                "Something went wrong! " +
-                  "Maybe you need to sign out and back in? " +
-                  "Check your browser console for more info."
-              );
-              throw e;
-            } finally {
-              // re-enable the form, whether the call succeeded or failed
-              fieldset.disabled = false;
-            }
-
-            // update local `greeting` variable to match persisted value
-            setGreeting(newGreeting);
-
-            // show Notification
-            setShowNotification(true);
-
-            // remove Notification again after css animation completes
-            // this allows it to be shown again next time the form is submitted
-            setTimeout(() => {
-              setShowNotification(false);
-            }, 11000);
-          }}
-        >
-          <fieldset id="fieldset">
-            <label
-              htmlFor="greeting"
-              style={{
-                display: "block",
-                color: "var(--gray)",
-                marginBottom: "0.5em",
-              }}
-            >
-              Change greeting
-            </label>
-            <div style={{ display: "flex" }}>
-              <input
-                autoComplete="off"
-                defaultValue={greeting}
-                id="greeting"
-                onChange={(e) => setButtonDisabled(e.target.value === greeting)}
-                style={{ flex: 1 }}
-              />
-              <button
-                disabled={buttonDisabled}
-                style={{ borderRadius: "0 5px 5px 0" }}
+        <h1>Welcome back, {window.accountId}</h1>
+        <Card>
+          <Card.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
               >
-                Save
-              </button>
-            </div>
-          </fieldset>
-        </form>
-        <ExampleHome />
+                <Form.Label>Greet yourself with a keyword</Form.Label>
+                <Form.Control
+                  placeholder={"current value: " + greeting}
+                  ref={greetingField}
+                  type="text"
+                  onChange={(e) =>
+                    setButtonDisabled(e.target.value === greeting)
+                  }
+                />
+              </Form.Group>
+            </Form>
+            <Button
+              disabled={buttonDisabled}
+              onClick={onSubmit}
+              variant="primary"
+            >
+              Greet
+            </Button>
+          </Card.Body>
+        </Card>
       </main>
       {showNotification && <ExampleNotification />}
-    </>
+    </BrowserRouter>
   );
 }

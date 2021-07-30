c87842fda7f3d612938279206abc883c72192ebe
Karen Bie <karen_bie@hotmail.com>
2021-07-30 00:04:51 -0400

    Test extracting React components into files

---
+++ src/Components/ExampleNotification.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import getConfig from "../config";
+
+const { networkId } = getConfig(process.env.NODE_ENV || "development");
+
+const ExampleNotification = (props) => {
+  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`;
+  return (
+    <aside>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`${urlPrefix}/${window.accountId}`}
+      >
+        {window.accountId}
+      </a>
+      {
+        " " /* React trims whitespace around tags; insert literal space character when needed */
+      }
+      called method: 'setGreeting' in contract:{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`${urlPrefix}/${window.contract.contractId}`}
+      >
+        {window.contract.contractId}
+      </a>
+      <footer>
+        <div>✔ Succeeded</div>
+        <div>Just now</div>
+      </footer>
+    </aside>
+  );
+};
+
+ExampleNotification.propTypes = {};
+
+export default ExampleNotification;

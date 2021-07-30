c56c165dfcdb144a33c26bcc578eed6a21bed99e
Karen Bie <karen_bie@hotmail.com>
2021-07-30 13:25:08 -0400

    Create HomePage component, refactor About -> AboutPage

--- src/Components/About.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const About = (props) => {
-  return <p>This is an About page.</p>;
-};
-
-About.propTypes = {};
-
-export default About;
---
+++ src/Components/AboutPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AboutPage = (props) => {
+  return <p>This is an About page.</p>;
+};
+
+AboutPage.propTypes = {};
+
+export default AboutPage;
